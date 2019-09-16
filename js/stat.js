480f60e250a922f74a43ef536638901051f77f44
Leonid163 <46902771+Leonid163@users.noreply.github.com>
2019-09-16 21:13:55 +0400

    2 задание(1)

--- js/stat.js
+++ js/stat.js
@@ -28,6 +28,7 @@ var getMaxElement = function (arr) {
 };
 
 
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
